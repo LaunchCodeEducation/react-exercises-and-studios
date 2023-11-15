12f7b1e538cc91018d832a3a2cc51529b4197bc6
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-11-14 20:53:18 -0600

    StudioReact1

---
+++ part1/exercises/src/Studio/src/components/Description.jsx/Photos.jsx
@@ -0,0 +1,7 @@
+import styles from './Description.module.css';
+
+const RecipePhoto = () => {
+    return <img src="https://hips.hearstapps.com/hmg-prod/images/monkey-bread-1671204935-641a011fc57cc.jpeg?crop=0.639xw:0.639xh;0.174xw,0.136xh&resize=1200:*" alt="Whitney's Monkey Bread" className = {styles.imageUpdates} />
+}
+
+export default RecipePhoto;
