4e07894a2fdd4f95155b3b0123251d1b5465aef8
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2023-11-20 18:13:12 -0600

    Add files via upload

---
+++ part3/exercises/HobbyIntroduction.jsx
@@ -0,0 +1,12 @@
+export default function HobbyIntroduction () {
+   return(
+      <div>
+         <h2>3 Fun Facts About Shopping</h2>
+         <ol>
+            <li>Shopping helps with my stress levels.</li>
+            <li>Shopping is fun and creative.</li>
+            <li>I love shopping for home decor and holiday decor.</li>
+         </ol>
+      </div>
+   );
+}
